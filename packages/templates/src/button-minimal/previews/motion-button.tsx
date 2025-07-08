d4eea98a51f1224e839c0c7d191e628ba22c3573
kodiyak <mathewsto51@gmail.com>
2025-07-08 07:18:45 -0300

    feat: implement MotionButton component with state management and motion effects

--- packages/templates/src/button-minimal/previews/motion-button.tsx
+++ packages/templates/src/button-minimal/previews/motion-button.tsx
@@ -1,15 +1,22 @@
 import * as motion from 'motion/react-client';
+import { useState } from 'react';
 import { Button } from '/index.tsx';
 
+const MButton = motion.create(Button);
+
 export default function MotionButton() {
+	const [count, setCount] = useState(0);
+
 	return (
-		<motion.div
+		<MButton
 			animate={{ opacity: 1, scale: 1, filter: 'blur(0px)' }}
-			className="flex flex-col"
 			initial={{ opacity: 0, scale: 0.9, filter: 'blur(5px)' }}
+			onClick={() => {
+				setCount((old) => old + 2);
+			}}
 			whileTap={{ scale: 0.9, filter: 'blur(5px)' }}
 		>
-			<Button>TrialsPreview 66111444Button</Button>
-		</motion.div>
+			My Count Button: {count}
+		</MButton>
 	);
 }
