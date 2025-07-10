44dff12d880fc39b72aedd035069feb23ef3c954
kodiyak <mathewsto51@gmail.com>
2025-07-10 14:08:18 -0300

    Implement refactoring of the codebase for improved readability and maintainability

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,3 +1,8 @@
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({});
+export default defineConfig({
+	plugins: [tsconfigPaths(), react()],
+	test: {},
+});
