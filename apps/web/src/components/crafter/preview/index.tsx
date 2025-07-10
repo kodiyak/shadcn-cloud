1e0b2f09c4029388c3621548f8e5ef62ce3181f3
kodiyak <mathewsto51@gmail.com>
2025-07-10 14:30:26 -0300

    feat: add CloudIcon and update Publish button layout in Preview component

--- apps/web/src/components/crafter/preview/index.tsx
+++ apps/web/src/components/crafter/preview/index.tsx
@@ -1,3 +1,5 @@
+import { CloudIcon } from '@phosphor-icons/react';
+import { Button } from '@workspace/ui/components/button';
 import DocsPreview from './docs-preview';
 
 export default function Preview() {
@@ -5,6 +7,12 @@ export default function Preview() {
 		<div className="size-full flex flex-col pl-1 pr-3 gap-4 py-3">
 			<div className="flex-1 relative border rounded-2xl bg-muted/30">
 				<DocsPreview />
+			</div>
+			<div className="flex justify-end">
+				<Button className="rounded-xl gap-2 group" size={'sm'}>
+					<span className="relative group-hover:animate-bounce">🚢</span>
+					<span>Publish</span>
+				</Button>
 			</div>
 		</div>
 	);
