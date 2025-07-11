a49a99b4137c005be0aa8b618751f791dda2d928
kodiyak <mathewsto51@gmail.com>
2025-07-11 12:23:16 -0300

    feat: enhance layout with footer links for navigation

--- apps/web/src/app/(public)/(nav)/layout.tsx
+++ apps/web/src/app/(public)/(nav)/layout.tsx
@@ -7,7 +7,7 @@ import {
 	TooltipContent,
 	TooltipTrigger,
 } from '@workspace/ui/components/tooltip';
-import { LibraryIcon } from 'lucide-react';
+import { LibraryIcon, PlusIcon } from 'lucide-react';
 import Link from 'next/link';
 import type { PropsWithChildren } from 'react';
 
@@ -22,11 +22,27 @@ export default function Page({ children }: PropsWithChildren) {
 		},
 		{ label: 'Bookmarks', href: '/bookmarks', icon: <BookmarksIcon /> },
 	];
+	const footerLinks = [{ label: 'Create', href: '/new', icon: <PlusIcon /> }];
 	return (
 		<>
-			<div className="fixed left-0 top-0 h-screen w-16 border-r">
+			<div className="fixed left-0 top-0 flex flex-col h-screen w-16 border-r">
 				<div className="flex flex-col items-center py-2">
 					{links.map((link) => (
+						<Tooltip delayDuration={0} key={link.href}>
+							<TooltipTrigger asChild>
+								<Button asChild size={'icon-lg'} variant={'ghost'}>
+									<Link href={link.href}>{link.icon}</Link>
+								</Button>
+							</TooltipTrigger>
+							<TooltipContent side={'right'}>
+								<p>{link.label}</p>
+							</TooltipContent>
+						</Tooltip>
+					))}
+				</div>
+				<div className="flex-1"></div>
+				<div className="flex flex-col items-center py-2">
+					{footerLinks.map((link) => (
 						<Tooltip delayDuration={0} key={link.href}>
 							<TooltipTrigger asChild>
 								<Button asChild size={'icon-lg'} variant={'ghost'}>
